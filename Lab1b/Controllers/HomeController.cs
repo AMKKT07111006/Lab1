bd1e3636d66433bab7eecac1d1b3f14aa4f634ef
AMKKT07111006 <lengophuckhang158@gmail.com>
2024-09-15 14:42:27 +0700

    Lab1b

---
+++ Lab1b/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1b.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Bai2ab()
+        {
+            return View();
+        }
+        public ActionResult Bai2c()
+        {
+            return View();
+        }
+        public ActionResult Bai3()
+        {
+            return View();
+        }
+        public ActionResult Bai4()
+        {
+            return View();
+        }
+        public ActionResult Bai5()
+        {
+            return View();
+        }
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
